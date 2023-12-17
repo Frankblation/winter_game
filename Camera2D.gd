7aede7790aa6cf3cbbd3adf7735d7c3574d13cc9
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-12-16 18:31:05 -0600

    pushing  fet

---
+++ Camera2D.gd
@@ -0,0 +1,16 @@
+#extends Camera2D
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass
+	#
+	 #
+#
+#@onready var target_sprite = $SnowEnemy/Sprite
+#var follow_speed : float = 200.0
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#if target_sprite:
+		#var target_position = get_node(target_sprite).global_position
+		#position.x = lerp(position.x, target_position.x, follow_speed * delta)
