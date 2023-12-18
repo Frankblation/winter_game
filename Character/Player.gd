c8a089845306089b43bb564ca49f01c860f77ae1
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-12-17 20:57:16 -0600

    pushing to git hub. character works.

--- Character/Player.gd
+++ Character/Player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 300.0
-@export var jump_velocity = -400.0
+@export var SPEED = 300.0
+@export var  JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -15,14 +16,15 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * SPEED
+#		anim.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
